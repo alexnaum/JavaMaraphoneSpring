0bf3b14915647da5866611d30cacd368344ecbd6
onaumenko <test@gmail.com>
2022-01-11 22:49:52 +0200

    create Spring application

---
+++ src/main/java/com/onaumenko/spring/javamaraphonespring/JavaMAraphoneSpringApplication.java
@@ -0,0 +1,13 @@
+package com.onaumenko.spring.javamaraphonespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaMAraphoneSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaMAraphoneSpringApplication.class, args);
+    }
+
+}
